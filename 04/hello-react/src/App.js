d9803649c28065ea81cf62025f20340000289460
changuk <dumonem@gmail.com>
2021-04-20 21:56:32 +0900

    4장 이벤트 핸들링

---
+++ 04/hello-react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import EventPractice from './EventPractice';
+// import MyComponent from './MyComponent';
+// import Counter from './Counter';
+// import Say from './Say';
+
+const App = () => {
+	return (
+		// <MyComponent name="React" favoriteNumber={1}>
+		// 	리액트
+		// </MyComponent>
+		// <Counter />
+		// <Say />
+		<EventPractice />
+	);
+};
+
+export default App;
