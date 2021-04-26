c41a359c3547a4492ad8599821aa1a37094b922d
changuk <dumonem@gmail.com>
2021-04-26 20:38:19 +0900

    11.7 TodoList 컴포넌트 최적화 하기

---
+++ 11/todo-app/src/components/TodoListItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './TodoListItem.scss';
+import cn from 'classnames';
+import {
+	MdCheckBox,
+	MdCheckBoxOutlineBlank,
+	MdRemoveCircleOutline,
+} from 'react-icons/all';
+
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+	const { id, text, checked } = todo;
+	return (
+		<div className="TodoListItem">
+			<div
+				className={cn('checkbox', { checked })}
+				onClick={() => {
+					onToggle(id);
+				}}
+			>
+				{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+				<div className="text">{text}</div>
+			</div>
+			<div
+				className="remove"
+				onClick={() => {
+					onRemove(id);
+				}}
+			>
+				<MdRemoveCircleOutline />
+			</div>
+		</div>
+	);
+};
+
+export default React.memo(TodoListItem);
